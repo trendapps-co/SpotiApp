d1d83333f1f7661933d5666e0ee58ab43c4ab600
Carlos Pinzon <druckern@gmail.com>
2018-12-11 07:28:36 -0500

    inclusion tabla usuarios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { SearchComponent } from './components/search/search.component';
 import { ArtistaComponent } from './components/artista/artista.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
+
+// Importar Rutas
+import { ROUTES } from './app.routes';
 
 @NgModule({
   declarations: [
@@ -16,7 +22,9 @@ import { NavbarComponent } from './components/shared/navbar/navbar.component';
     NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot( ROUTES, { useHash: true } )
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { element } from 'protractor';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+ usuarios: Usuario[] = [];
+ info:any[]=[];
+
+  constructor( private http: HttpClient) {
+    this.http.get('https://getmystyle.herokuapp.com/Users')
+    .subscribe( (resp: any ) => {
+      this.info = resp.message;
+      for (const key in this.info) {
+        if (this.info.hasOwnProperty(key)) {
+          const element = this.info[key];
+          // console.log(element.email);
+          this.usuarios.push({name: element.name, email: element.email});
+        }
+      }
+
+
+    });
+
+    console.log(this.usuarios);
+   }
 
   ngOnInit() {
   }
 
+
+}
+
+export interface Usuario {
+  name: string;
+  email: string;
 }
